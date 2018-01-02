02ad270fc5ecc5424e893548efe30396cd1c7f8b
Leonard Thieu <leonard-thieu@users.noreply.github.com>
2018-01-02 14:37:48 -0500

    chore(build): Update toofz.Build to v2.1.1. (#15)

--- src/toofz.NecroDancer/Properties/AssemblyInfo.cs
+++ src/toofz.NecroDancer/Properties/AssemblyInfo.cs
@@ -1,14 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyVersion("5.0.5.0")]
-
-[assembly: AssemblyCopyright("Copyright © Leonard Thieu 2017")]
-[assembly: AssemblyProduct("toofz")]
-
-[assembly: AssemblyTitle("toofz NecroDancer Core")]
-
-[assembly: ComVisible(false)]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("toofz.NecroDancer.Tests")]
